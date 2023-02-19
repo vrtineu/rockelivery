096c12ac1217e1f74a25dafc892d6802970df621
vrtineu <vininro@gmail.com>
2023-02-19 14:56:06 -0300

    feat: creating items schema

---
+++ lib/rockelivery/item.ex
@@ -0,0 +1,34 @@
+defmodule Rockelivery.Item do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:category, :description, :price, :photo]
+  @items_categories [:pizza, :drink, :dessert]
+
+  @derive {Jason.Encoder, only: @required_params ++ [:id]}
+
+  schema "items" do
+    field :category, Ecto.Enum, values: @items_categories
+    field :description, :string
+    field :price, :decimal
+    field :photo, :string
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
+    |> changes(@required_params, params)
+  end
+
+  defp changes(struct, fields, params) do
+    struct
+    |> cast(params, fields)
+    |> validate_required(fields)
+    |> validate_length(:description, min: 6)
+    |> validate_number(:price, greater_than: 0)
+  end
+end
