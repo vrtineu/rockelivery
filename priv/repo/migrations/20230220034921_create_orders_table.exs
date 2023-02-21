bc764a62a0fb5fbde2ab9445c93be0e54d164029
vrtineu <vininro@gmail.com>
2023-02-21 20:20:04 -0300

    chore: adjusting field name

--- priv/repo/migrations/20230220034921_create_orders_table.exs
+++ priv/repo/migrations/20230220034921_create_orders_table.exs
@@ -3,7 +3,7 @@ defmodule Rockelivery.Repo.Migrations.CreateOrdersTable do
 
   def change do
     create table(:orders) do
-      add :adress, :string
+      add :address, :string
       add :comments, :string
       add :payment_method, :payment_method
       add :user_id, references(:users, type: :binary_id)
