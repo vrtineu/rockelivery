78269d56f672d1c618b9563108e3c548f9ff6b1d
vrtineu <vininro@gmail.com>
2023-02-22 03:14:57 -0300

    refactor: removing side logic from principal module

--- lib/rockelivery/orders/create.ex
+++ lib/rockelivery/orders/create.ex
@@ -2,52 +2,28 @@ defmodule Rockelivery.Orders.Create do
   import Ecto.Query
 
   alias Rockelivery.{Error, Item, Order, Repo}
+  alias Rockelivery.Orders.ValidateAndMultiplyItems
 
   def call(params) do
-    with {:ok, items_multiplied} <- fetch_items(params),
-         {:ok, %Order{}} = order <- insert_order(items_multiplied, params) do
+    with {:ok, multiplied_items} <- fetch_items(params),
+         {:ok, %Order{}} = order <- insert_order(multiplied_items, params) do
       order
     else
-      {:error, result} -> {:error, Error.build(:bad_request, result)}
+      {:error, reason} -> {:error, Error.build(:bad_request, reason)}
     end
   end
 
-  defp fetch_items(%{"items" => items}) do
-    ids = Enum.map(items, & &1["id"])
-    query = from item in Item, where: item.id in ^ids
+  defp fetch_items(%{"items" => params_items}) do
+    ids_items = Enum.map(params_items, & &1["id"])
+    query = from item in Item, where: item.id in ^ids_items
 
     query
     |> Repo.all()
-    |> check_invalid_items(ids)
-    |> multiply_items(items)
+    |> ValidateAndMultiplyItems.call(ids_items, params_items)
   end
 
-  defp check_invalid_items(items, ids) do
-    map_items = Map.new(items, &{&1.id, &1})
-
-    ids
-    |> Enum.map(fn id -> {id, Map.get(map_items, id)} end)
-    |> Enum.any?(fn {_id, value} -> is_nil(value) end)
-    |> case do
-      true -> {:error, "There are invalid ids"}
-      false -> {:ok, map_items}
-    end
-  end
-
-  defp multiply_items({:error, result}, _items), do: {:error, result}
-
-  defp multiply_items({:ok, map_items}, items) do
-    items_multiplied =
-      Enum.reduce(items, [], fn %{"id" => id, "quantity" => quantity}, acc ->
-        item = Map.get(map_items, id)
-        acc ++ List.duplicate(item, quantity)
-      end)
-
-    {:ok, items_multiplied}
-  end
-
-  defp insert_order(items, params) do
-    params
+  defp insert_order(items, params_items) do
+    params_items
     |> Order.changeset(items)
     |> Repo.insert()
   end
---
+++ lib/rockelivery/orders/validate_and_multiply_items.ex
@@ -0,0 +1,36 @@
+defmodule Rockelivery.Orders.ValidateAndMultiplyItems do
+  def call(query_items, ids, params_items) do
+    with {:ok, _mapped_items} = mapped_items <- check_invalid_ids(query_items, ids),
+         {:ok, _multiplied_items} = multiplied_items <- multiply_items(mapped_items, params_items) do
+      multiplied_items
+    else
+      {:error, _reason} = error -> error
+    end
+  end
+
+  defp check_invalid_ids(query_items, ids) do
+    mapped_items = Map.new(query_items, &{&1.id, &1})
+
+    invalid_ids =
+      ids
+      |> Enum.map(fn id -> {id, Map.get(mapped_items, id)} end)
+      |> Enum.any?(fn {_id, value} -> is_nil(value) end)
+
+    case invalid_ids do
+      true -> {:error, "Invalid item ids"}
+      false -> {:ok, mapped_items}
+    end
+  end
+
+  defp multiply_items({:error, reason}, _items), do: {:error, reason}
+
+  defp multiply_items({:ok, mapped_items}, params_items) do
+    items_multiplied =
+      Enum.reduce(params_items, [], fn %{"id" => id, "quantity" => quantity}, acc ->
+        item = Map.get(mapped_items, id)
+        acc ++ List.duplicate(item, quantity)
+      end)
+
+    {:ok, items_multiplied}
+  end
+end
